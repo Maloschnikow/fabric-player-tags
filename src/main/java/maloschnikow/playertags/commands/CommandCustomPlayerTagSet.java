31ccaa204c0ad51eaab3e5e5665f56881be51e72
Maloschnikow <123018032+Maloschnikow@users.noreply.github.com>
2025-10-01 19:35:31 +0200

    add feedback message

--- src/main/java/maloschnikow/playertags/commands/CommandCustomPlayerTagSet.java
+++ src/main/java/maloschnikow/playertags/commands/CommandCustomPlayerTagSet.java
@@ -13,6 +13,7 @@ import net.minecraft.scoreboard.Team;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
@@ -33,20 +34,29 @@ public class CommandCustomPlayerTagSet implements Command<ServerCommandSource> {
         if( (!commandSource.hasPermissionLevel(2)) && player != commandSource.getPlayer() ) {
             throw new CommandSyntaxException(null, new WrongPermissionLevelMessage(2, "change another player's tag"));
         }
-        
-        Text teamPrefix   = Text.literal("[").formatted(Formatting.GRAY).append(
+
+        MutableText teamPrefix   = Text.literal("[").formatted(Formatting.GRAY).append(
                             Text.literal(tagString).formatted(color)).append(
-                            Text.literal("] ").formatted(Formatting.GRAY));
-                
+                            Text.literal("]").formatted(Formatting.GRAY));
+        
+        //prepare success message here, so the old display name is still shown
+        Text sourceSuccessMsg = player.getDisplayName().copy().append(Text.literal("'s new tag is ")).append(teamPrefix).append(".");
+        Text targetSuccessMsg = Text.literal("Your new tag is ").append(teamPrefix).append(".");
+
         String playerUUID = player.getUuidAsString();
         Team playerTeam = scoreboard.getTeam(playerUUID);
         if(playerTeam != null) {
             scoreboard.removeTeam(playerTeam);
         }
         playerTeam = scoreboard.addTeam(playerUUID);
-        playerTeam.setPrefix(teamPrefix);
+        playerTeam.setPrefix(teamPrefix.append(Text.literal(" ").formatted(Formatting.RESET)));
         
         scoreboard.addScoreHolderToTeam(player.getName().getString(), playerTeam);
+
+        if(commandSource.getPlayer() != player) {
+            commandSource.sendMessage(sourceSuccessMsg);
+        }
+        player.sendMessage(targetSuccessMsg);
         
         return Command.SINGLE_SUCCESS;
     }
